ca1b3eb1055f56cfa193c2ab7ba38bc52871a493
gustavo.aguiar <gustavo.aguiar@redspark.io>
2023-12-06 10:29:04 -0300

    configurando caminho relativo alias

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,37 @@
-module.exports = function(api) {
+/* eslint-disable no-undef */
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    env: {
+      development: {
+        plugins: [
+          [
+            "module-resolver",
+            {
+              root: ["./src"],
+              alias: {
+                "@": "./src",
+              },
+            },
+          ],
+          "inline-dotenv",
+        ],
+      },
+      production: {
+        plugins: [
+          [
+            "module-resolver",
+            {
+              root: ["./src"],
+              alias: {
+                "@": "./src",
+              },
+            },
+          ],
+          "inline-dotenv",
+        ],
+      },
+    },
   };
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Test } from "@/screens/Test";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>web FALA DEVDOIDO Open up App.js to start working on your app!</Text>
+      <Text>wedddb FALA DEVDOIDO Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Test />
     </View>
   );
 }
