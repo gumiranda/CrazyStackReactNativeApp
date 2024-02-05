58e0991e1aadd15451ddb97b6dbe9fdff903f5d3
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-02-05 11:25:29 -0300

    Adiciona dependências e rotas iniciais

---
+++ src/shared/libs/navigation/Routes.tsx
@@ -0,0 +1,10 @@
+import { Initial } from "@/screens/Initial";
+
+export const stackRoutes = [
+  {
+    name: "Initial",
+    component: Initial,
+    title: "Início",
+  },
+];
+export const routesWithoutHeader = ["Initial"];
