d67033a5ecefd08da8dbbc4362f84656b6e8696f
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-02-05 10:32:30 -0300

    Adicionado arquivo theme.ts com definições de tema escuro e claro

---
+++ src/shared/libs/utils/theme.ts
@@ -0,0 +1,23 @@
+import { Appearance, ColorSchemeName } from "react-native";
+import { darkColors, lightColors } from "./colors";
+export const darkTheme = {
+  dark: true,
+  colors: darkColors,
+  // Add more style definitions for the dark theme
+};
+
+export const lightTheme = {
+  dark: false,
+  colors: lightColors,
+  // Add more style definitions for the light theme
+};
+export const getNativeTheme = () => {
+  return Appearance.getColorScheme();
+};
+export const getTheme = (theme: ColorSchemeName) => {
+  return theme === "dark" ? darkTheme : lightTheme;
+};
+export const useTheme = () => {
+  const theme = Appearance.getColorScheme();
+  return getTheme(theme);
+};
