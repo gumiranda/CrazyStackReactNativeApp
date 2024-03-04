72760ac4e7a92d6d8764996ed889126118cdfcfb
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-03-04 17:44:07 -0300

    Adiciona botões "REAGENDAR" e "CANCELAR" na tela de detalhes das minhas solicitações do proprietário

--- src/screens/request/details/MyRequestsDetailsOwner.tsx
+++ src/screens/request/details/MyRequestsDetailsOwner.tsx
@@ -1,8 +1,10 @@
-import { DynamicStyleSheet } from "@/shared/libs/utils";
+import { DynamicStyleSheet, useTheme } from "@/shared/libs/utils";
 import { ScrollView, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { RequestDetails } from "@/entities/request/components";
 import { useRequestDetailsOwner } from "./useRequestDetailsOwner";
+import { Button } from "@/shared/ui";
+import appMetrics from "@/shared/libs/functions/metrics";
 
 export const MyRequestsDetailsOwner = ({
   route: {
@@ -10,6 +12,7 @@ export const MyRequestsDetailsOwner = ({
   },
 }) => {
   const { serviceId, clientId } = item;
+  const theme = useTheme();
   const { service, client } = useRequestDetailsOwner({ serviceId, clientId });
   return (
     <View style={styles.container}>
@@ -22,6 +25,20 @@ export const MyRequestsDetailsOwner = ({
           }}
         />
       </ScrollView>
+      <Button
+        style={styles.button}
+        onPress={() => {}}
+        title={"REAGENDAR"}
+        backgroundColor={theme.colors.background}
+        color={theme.colors.primary[400]}
+      />
+      <Button
+        style={styles.buttonError}
+        onPress={() => {}}
+        title={"CANCELAR"}
+        backgroundColor={theme.colors.background}
+        color={theme.colors.error[400]}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -32,5 +49,19 @@ const styles = DynamicStyleSheet.create((theme) => ({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    width: appMetrics.SCREEN_WIDTH * 0.9,
+    alignSelf: "center",
+    borderWidth: 1,
+    borderColor: theme.colors.primary[400],
+    marginBottom: 30,
+  },
+  buttonError: {
+    width: appMetrics.SCREEN_WIDTH * 0.9,
+    alignSelf: "center",
+    borderWidth: 1,
+    borderColor: theme.colors.error[400],
+    marginBottom: 60,
   },
 }));
