e2a8f1200a9a4fef36f2e55832557ce0ef270238
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-05-07 17:41:12 -0300

    feat: Add email, ownerId, role, myOwnerId, createdById, serviceIds, daysSinceRegister, and phone properties to User model

--- src/slices/general/entities/user/user.model.ts
+++ src/slices/general/entities/user/user.model.ts
@@ -1,9 +1,18 @@
+/* eslint-disable indent */
 export type UserProps = {
   _id: string;
   name: string;
+  email: string;
+  ownerId: string;
+  role: string;
+  myOwnerId: string;
+  createdById: string;
   createdAt: string;
   value?: boolean;
   active?: boolean;
+  serviceIds?: string[];
+  daysSinceRegister?: number;
+  phone?: string;
 };
 
 class User {
@@ -26,6 +35,33 @@ class User {
   get active(): boolean | undefined {
     return this.props.active;
   }
+  get serviceIds(): string[] | undefined {
+    return this.props.serviceIds;
+  }
+  get email(): string {
+    return this.props.email;
+  }
+  get role(): string {
+    return this.props.role;
+  }
+  get ownerId(): string {
+    return this.props.ownerId;
+  }
+  get myOwnerId(): string {
+    return this.props.myOwnerId;
+  }
+  get createdById(): string {
+    return this.props.createdById;
+  }
+  get value(): boolean | undefined {
+    return this.props.value;
+  }
+  get daysSinceRegister(): number | undefined {
+    return this.props.daysSinceRegister;
+  }
+  get phone(): string | undefined {
+    return this.props.phone;
+  }
   format(): UserProps {
     return {
       ...this.props,
@@ -33,12 +69,20 @@ class User {
       name: this.props.name,
       active: this.props.active,
       value: false,
-      createdAt: new Date(this.props.createdAt).toLocaleDateString("pt-BR", {
-        day: "2-digit",
-        month: "2-digit",
-        year: "numeric",
-      }),
+      createdAt: this.props.createdAt,
+      daysSinceRegister: this.props.createdAt
+        ? Math.floor(
+            (new Date().getTime() - new Date(this.props.createdAt).getTime()) /
+              (1000 * 60 * 60 * 24)
+          )
+        : 0,
     };
   }
 }
+export const calculateDaysSinceRegister = (createdAt: string) => {
+  if (!createdAt) return 9999;
+  return Math.floor(
+    (new Date().getTime() - new Date(createdAt).getTime()) / (1000 * 60 * 60 * 24)
+  );
+};
 export const userModel = (props: UserProps) => User.build(props);
