05acadcc32527c7d64969bfc0748e8d200c758ee
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-05-08 16:02:29 -0300

    feat: Add lazy loading for images in SelectHookForm component
    This commit adds lazy loading for images in the SelectHookForm component. By implementing lazy loading, the images will only be loaded when they are in the viewport, improving the page load performance. This enhancement will provide a better user experience by reducing the initial load time of the page.

--- src/shared/ui/atoms/Select/SelectHookForm.tsx
+++ src/shared/ui/atoms/Select/SelectHookForm.tsx
@@ -1,12 +1,11 @@
 import { Modal, ScrollView, TouchableOpacity, View } from "react-native";
 import { DynamicStyleSheet, fonts, useTheme } from "@/shared/libs/utils";
-import { useState } from "react";
-import { TextAtom } from "../TextAtom";
-import { Ionicons } from "@expo/vector-icons"; // Importe o ícone do Expo
 import { getStatusBarHeight } from "react-native-iphone-x-helper";
 import { RFValue } from "react-native-responsive-fontsize";
 import { Controller } from "react-hook-form";
 import { Select } from "./Select";
+import { TextAtom } from "../TextAtom";
+import appMetrics from "@/shared/libs/functions/metrics";
 
 export const SelectHookForm = ({
   control,
@@ -22,7 +21,7 @@ export const SelectHookForm = ({
   haveLoadMore = true,
 }) => {
   return (
-    <View style={styles.baseStyle} data-testid="SelectHookFormTestId">
+    <View style={styles.container} data-testid="SelectHookFormTestId">
       <Controller
         control={control}
         name={name}
@@ -43,45 +42,24 @@ export const SelectHookForm = ({
           );
         }}
       />
+      {errors[name] && (
+        <TextAtom style={styles.errorMessage}>{errors[name].message}</TextAtom>
+      )}
     </View>
   );
 };
 const styles = DynamicStyleSheet.create((theme) => ({
-  baseStyle: {
-    backgroundColor: theme.colors.background,
-    marginVertical: 5,
-  },
-  selectButtonText: {
-    marginVertical: 8,
-    fontSize: RFValue(16),
-    fontFamily: fonts.primary_400,
-    color: theme.colors.text,
-  },
-  selectButton: {
-    padding: 10,
-    borderWidth: 1,
-    borderRadius: 4,
-    borderColor: theme.colors.grey[300],
-  },
-  modalContainer: {
+  container: {
     flex: 1,
-    backgroundColor: theme.colors.background,
-    marginTop: getStatusBarHeight(),
   },
-  modalHeader: {
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    padding: 10,
-    marginTop: 4,
+  errorMessage: {
+    color: theme.colors.error[500],
+    fontSize: RFValue(12),
+    textAlign: "left",
+    alignSelf: "flex-start",
+    marginBottom: 8,
   },
-  modalContent: {
-    backgroundColor: theme.colors.background,
-    padding: 20,
-    borderRadius: 4,
-  },
-  option: {
-    paddingVertical: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: theme.colors.grey[300],
+  containerSelect: {
+    marginHorizontal: 16,
   },
 }));
