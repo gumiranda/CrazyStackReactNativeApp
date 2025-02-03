0ab6865c15750c43ff4b3c16d3d2fb0ab895157b
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-02-03 10:32:47 -0300

    Adicionar exibição de mapa e marcadores para mercados na tela HomeClient

--- src/slices/appointments/screens/home/HomeClient.tsx
+++ src/slices/appointments/screens/home/HomeClient.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { Alert, View, Text } from "react-native";
 import MapView, { Callout, Marker } from "react-native-maps";
 import * as Location from "expo-location";
+import { fonts, useTheme } from "@/shared/libs/utils";
 
 export type CategoryProps = {
   id: string;
@@ -22,6 +23,7 @@ type MarketProps = PlaceProps & {
 };
 
 export const HomeClient = () => {
+  const theme = useTheme();
   const [categories, setCategories] = useState<CategoryProps>([]);
   const [category, setCategory] = useState<string>("");
   const [markets, setMarkets] = useState<MarketProps[]>([]);
@@ -71,5 +73,70 @@ export const HomeClient = () => {
       Alert.alert("Erro", "Não foi possível carregar os mercados");
     }
   }
-  return <></>;
+  return (
+    <View style={{ flex: 1, backgroundColor: "#cecece" }}>
+      {/* <Categories
+        data={categories}
+        selected={category}
+        onSelect={(id) => setCategory(id)}
+      /> */}
+      {currentLocation && (
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            identifier="current"
+            coordinate={currentLocation}
+            image={require("@/assets/location.png")}
+          />
+          {markets.map((market) => (
+            <Marker
+              key={market.id}
+              identifier={market.id}
+              coordinate={{
+                latitude: market.latitude,
+                longitude: market.longitude,
+              }}
+              image={require("@/assets/pin.png")}
+            >
+              <Callout
+                onPress={() => {
+                  // @ts-ignore
+                  router.navigate(`market/${market.id}`);
+                }}
+              >
+                <View>
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      color: theme.colors.gray[600],
+                      fontFamily: fonts.poppins_500,
+                    }}
+                  >
+                    {market.name}
+                  </Text>
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      color: theme.colors.gray[600],
+                      fontFamily: fonts.poppins_400,
+                    }}
+                  >
+                    {market.address}
+                  </Text>
+                </View>
+              </Callout>
+            </Marker>
+          ))}
+        </MapView>
+      )}
+      {/* <Places data={markets} /> */}
+    </View>
+  );
 };
