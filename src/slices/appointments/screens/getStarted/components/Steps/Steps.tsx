7701adfa829d713325f04af8762021beda5d94b4
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-03-21 16:25:16 -0300

    Implementar componentes de passos na tela "GetStarted", incluindo a estrutura e estilos para exibir etapas com ícones e descrições. Refatorar o componente "Step" para receber propriedades e renderizar informações de forma dinâmica.

--- src/slices/appointments/screens/getStarted/components/Steps/Steps.tsx
+++ src/slices/appointments/screens/getStarted/components/Steps/Steps.tsx
@@ -1,3 +1,36 @@
+import { DynamicStyleSheet, fonts } from "@/shared/libs/utils";
+import { IconMapPin, IconQrcode, IconTicket } from "@tabler/icons-react-native";
+import { ScrollView, Text } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { Step } from "./Step/Step";
+
 export const Steps = () => {
-  return <></>;
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Veja como funciona:</Text>
+      <Step
+        title={"Encontre estabelecimentos"}
+        description={"Veja locais perto de voce que são parceiros Belezix"}
+        icon={IconMapPin}
+      />
+      <Step
+        title={"Ative o cupom com QR Code"}
+        description={"Escaneie o código no estabelecimento para usar o benefício"}
+        icon={IconQrcode}
+      />
+      <Step
+        title={"Garanta vantagens perto de você"}
+        description={"Ative cupons onde estiver, em diferentes tipos de estabelecimento"}
+        icon={IconTicket}
+      />
+    </ScrollView>
+  );
 };
+const styles = DynamicStyleSheet.create((theme) => ({
+  container: { flex: 1, gap: 24 },
+  title: {
+    fontSize: RFValue(14),
+    fontFamily: fonts.poppins_500,
+    color: theme.colors.gray[700],
+  },
+}));
