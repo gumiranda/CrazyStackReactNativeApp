3e3e6a75c122ce6a8f02132c09cbf368f7747101
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-03-21 10:20:59 -0300

    Adicionar arquivo de configuração do Expo, substituir app.json por app.config.js, atualizar versões do Gradle e dependências do Podfile, e implementar novos ícones e telas de splash para o aplicativo.

---
+++ app.config.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv").config(); // Carrega variáveis do .env
+
+export default {
+  expo: {
+    name: "crazystack",
+    slug: "crazystack",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: false,
+    assetBundlePatterns: ["**/*"],
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    packagerOpts: {
+      config: "metro.config.js",
+    },
+    splash: {
+      image: "./assets/images/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#8826d5",
+    },
+    android: {
+      package: "br.com.crazystack",
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY, // Lê do .env
+        },
+      },
+    },
+    ios: {
+      bundleIdentifier: "br.com.crazystack",
+      supportsTablet: true,
+      infoPlist: {
+        NSCameraUsageDescription:
+          "We need access to your camera to allow you to take photos and videos within the app.",
+      },
+    },
+  },
+};
