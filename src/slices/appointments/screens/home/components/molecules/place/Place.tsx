186702306119d60f63230f1c3a37a2d045d2e7e8
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-03-22 08:22:02 -0300

    Adicionar componente "Places" para exibição de locais em um BottomSheet, integrando o componente "Place" para visualização detalhada. Atualizar estilos e propriedades para melhor apresentação e navegação entre locais.

--- src/slices/appointments/screens/home/components/molecules/place/Place.tsx
+++ src/slices/appointments/screens/home/components/molecules/place/Place.tsx
@@ -7,44 +7,68 @@ import {
 } from "react-native";
 import { IconTicket } from "@tabler/icons-react-native";
 import { DynamicStyleSheet, fonts, useTheme } from "@/shared/libs/utils";
-type PlaceProps = {
-  _id: string;
-  name: string;
-  description: string;
-  coupons: number;
-  cover: string;
-  address: string;
-};
+import type { PlaceProps } from "../../organisms/places";
+import { RFValue } from "react-native-responsive-fontsize";
+
 type Props = TouchableOpacityProps & {
   data: PlaceProps;
 };
 
 export function Place({ data, ...rest }: Props) {
   const theme = useTheme();
-  return <></>;
+  return (
+    <TouchableOpacity style={styles.container} {...rest}>
+      <Image style={styles.image} source={{ uri: data?.cover }} />
+      <View style={styles.content}>
+        <Text style={styles.name}>{data?.name}</Text>
+        <Text style={styles.description}>{data?.description}</Text>
+        <View style={styles.footer}>
+          <IconTicket size={24} color={theme.colors.gray[700]} />
+          <Text style={styles.footerText}>{data?.coupons ?? 0} disponíveis</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
 }
 
 const styles = DynamicStyleSheet.create((theme) => ({
   container: {
-    height: 36,
+    height: 120,
     backgroundColor: theme.colors.gray[100],
+    width: "100%",
+    padding: 8,
     borderWidth: 1,
-    borderColor: theme.colors.gray[300],
-    borderRadius: 8,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 12,
+    borderColor: theme.colors.gray[200],
+    borderRadius: 12,
+    gap: 16,
     flexDirection: "row",
-    gap: 10,
+    alignItems: "center",
+  },
+  image: {
+    width: 116,
+    height: 104,
+    borderRadius: 8,
+    backgroundColor: theme.colors.gray[200],
   },
   name: {
-    fontSize: 14,
+    fontSize: RFValue(13),
+    fontFamily: fonts.primary_500,
     color: theme.colors.gray[700],
+  },
+  description: {
+    fontSize: RFValue(11),
     fontFamily: fonts.primary_400,
+    color: theme.colors.gray[600],
   },
-  containerSelected: {
-    backgroundColor: theme.colors.primary[500],
-    borderColor: theme.colors.primary[500],
+  footer: {
+    flexDirection: "row",
+    gap: 8,
+    marginTop: 10,
   },
-  nameSelected: { color: theme.colors.grey[100] },
+  footerText: {
+    fontSize: RFValue(10),
+    fontFamily: fonts.primary_400,
+    color: theme.colors.gray[600],
+  },
+  content: { flex: 1, gap: 4 },
 }));
